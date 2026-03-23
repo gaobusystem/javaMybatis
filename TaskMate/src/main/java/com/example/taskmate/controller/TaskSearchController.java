247664e24438e071ce4e459197b0824cf2ef1b69
yamato2323 <dev@gaobusystem.com>
2026-03-24 08:52:28 +0900

    12　タスク検索条件

--- TaskMate/src/main/java/com/example/taskmate/controller/TaskSearchController.java
+++ TaskMate/src/main/java/com/example/taskmate/controller/TaskSearchController.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.taskmate.entity.Status;
 import com.example.taskmate.entity.TaskSummary;
+import com.example.taskmate.form.TaskSearchListForm;
+import com.example.taskmate.service.StatusService;
 import com.example.taskmate.service.TaskService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,19 +23,41 @@ import lombok.RequiredArgsConstructor;
 public class TaskSearchController {
 	
 	private final TaskService taskService;
+	private final StatusService statusService;
 
 	/*--- 最初のリクエスト -------------------------------*/
 	@GetMapping("/top")
-	private String showListSelection() {
+	private String showListSelection(
+			@ModelAttribute TaskSearchListForm form,
+			Model model) {
+		
+		// ステータスリストを Model に設定
+		List<Status> list = statusService.findAll();
+		model.addAttribute("statusList", list);
+
 		// HTMLテンプレート名で return
 		return "task-list";
 	}
 
 	/*--- 一覧検索リクエスト -------------------------------*/
 	@PostMapping("/task-search-list")
-	private String searchList(Model model) {
+	private String searchList(
+			@Validated @ModelAttribute TaskSearchListForm form,
+			BindingResult result,
+			Model model) {
+		
+		// form内容の表示
+		System.out.println("---searchList---");
+		System.out.println(form);
+		
+		// 全件検索ー＞条件検索に変更する
+		
 		// 一覧の全件検索
 		List<TaskSummary> list = taskService.findListAll();
+		
+		// ステータスリストを Model に設定（次回検索用）
+		List<Status> statusList = statusService.findAll();
+		model.addAttribute("statusList", statusList);
 		
 		// 結果を格納して HTMLテンプレート名で reutrn
 		model.addAttribute("taskSummaryList", list);
