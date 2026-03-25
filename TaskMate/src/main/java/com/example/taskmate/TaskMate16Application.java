f038356b031da46ee9a8dd19eea7309adc5f584b
yamato2323 <dev@gaobusystem.com>
2026-03-26 08:48:30 +0900

    16　タスク編集画面の流れ

---
+++ TaskMate/src/main/java/com/example/taskmate/TaskMate16Application.java
@@ -0,0 +1,13 @@
+package com.example.taskmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMate16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMate16Application.class, args);
+	}
+
+}
