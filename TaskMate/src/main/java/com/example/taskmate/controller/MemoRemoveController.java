9f4759e51221513825f75eeb87c3e0f09676bf73
yamato2323 <dev@gaobusystem.com>
2026-03-26 09:04:56 +0900

    21　メモ削除の流れ

---
+++ TaskMate/src/main/java/com/example/taskmate/controller/MemoRemoveController.java
@@ -0,0 +1,44 @@
+package com.example.taskmate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.taskmate.form.MemoRemoveForm;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MemoRemoveController {
+
+	/*--- メモ削除リクエスト（メモ一覧画面より） ---*/
+	@PostMapping("/memo-remove")
+	public String remove(
+			@ModelAttribute MemoRemoveForm form) {
+
+		// 正常な場合に メモ登録確認画面に遷移する
+		return "memo-confirm-remove";
+	}
+
+	/*--- メモ削除リクエスト（削除確認画面より） ---*/
+	@PostMapping("/memo-confirm-remove")
+	public String confirmRemove(
+			@ModelAttribute MemoRemoveForm form,
+			RedirectAttributes redirectAttributes) {
+
+		//----とりあえず表示
+		System.out.println("---メモ削除");
+		System.out.println(form);
+
+		//--- ここで削除実行処理
+		
+		// フラッシュスコープに完了メッセージを表示して リダイレクト
+		redirectAttributes.addFlashAttribute("msg", "(メモ削除)");
+		redirectAttributes.addFlashAttribute("taskId", form.getTaskId());
+		
+		return "redirect:/memo-complete";
+	}
+
+}
